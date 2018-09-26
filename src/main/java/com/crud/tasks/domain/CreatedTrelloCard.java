0dac772e45b53dd8b8996ce6462318726bbd9b3d
Joanna Grobelak <joannagrobelak@wp.pl>
2018-09-26 10:04:36 +0200

    module 18.4

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -18,6 +18,6 @@ public class CreatedTrelloCard {
     private String name;
     @JsonProperty("shortUrl")
     private String shortUrl;
-    @JsonProperty("badges")
-    private Badges badges;
+    //@JsonProperty("badges")
+    //private Badges badges;
 }
