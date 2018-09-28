04c8c8a49665fd86525394c2458fca5ddaad578a
Joanna Grobelak <joannagrobelak@wp.pl>
2018-09-28 09:34:42 +0200

    module 19.3

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Mail {
+
+    private String mailTo;
+    private String subject;
+    private String message;
+}
